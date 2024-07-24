06e66c521504b6525772a450f18b090bf86d11da
Mirik9724 <145905797+Mirik9724@users.noreply.github.com>
2024-07-24 16:47:07 +1000

    Add files via upload

--- loadComponent.js
+++ loadComponent.js
@@ -10,3 +10,10 @@ document.addEventListener("DOMContentLoaded", function() {
     loadComponent('header.html', 'header-placeholder');
     loadComponent('footer.html', 'footer-placeholder');
 });
+
+function showRickRollVideo() {
+            var video = document.getElementById('video-player');
+            video.style.display = 'block'; // Показать видео
+            video.play(); // Запустить воспроизведение
+            video.volume = 1.0; // Установить максимальную громкость
+        }
