f5e0f9268485fbc1d4e838cf3dc114ae588c9484
Mirik9724 <145905797+Mirik9724@users.noreply.github.com>
2024-10-16 20:40:13 +1000

    Update loadComponent.js

--- loadComponent.js
+++ loadComponent.js
@@ -1,11 +1,13 @@
-function loadComponent(url, elementId){
+function loadComponent(url, elementId) {
     fetch(url)
         .then(response => response.text())
         .then(data => {
             document.getElementById(elementId).innerHTML = data;
-        });
+        })
+        .catch(error => console.error('Ошибка при загрузке компонента:', error));
 }
 
+// Загружаем хедер и футер после загрузки страницы
 document.addEventListener("DOMContentLoaded", function() {
     loadComponent('header.html', 'header-placeholder');
     loadComponent('footer.html', 'footer-placeholder');
