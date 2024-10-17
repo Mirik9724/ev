4d89d13f76a984a019cf4957d6375356f523e483
Mirik9724 <145905797+Mirik9724@users.noreply.github.com>
2024-10-17 14:02:28 +1000

    Update loadComponent.js

--- loadComponent.js
+++ loadComponent.js
@@ -1,23 +1,9 @@
-function loadComponent(url, elementId) {
+function loadComponent(url, elementId){
     fetch(url)
         .then(response => response.text())
         .then(data => {
-            const container = document.getElementById(elementId);
-            container.innerHTML = data;
-
-            // Выполнение скриптов
-            const scripts = container.getElementsByTagName('script');
-            for (let i = 0; i < scripts.length; i++) {
-                const script = document.createElement('script');
-                if (scripts[i].src) {
-                    script.src = scripts[i].src;
-                } else {
-                    script.innerHTML = scripts[i].innerHTML;
-                }
-                document.body.appendChild(script); // Вставляем скрипт в тело страницы
-            }
-        })
-        .catch(error => console.error('Ошибка при загрузке компонента:', error));
+            document.getElementById(elementId).innerHTML = data;
+        });
 }
 
 document.addEventListener("DOMContentLoaded", function() {
